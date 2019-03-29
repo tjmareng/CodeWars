1da34fc4edeab7ee57a6d1829844a54ff8e40866
Tyler Marenger <tjmareng@mtu.edu>
2019-03-29 11:37:56 -0400

    Completed opposite Challenge

---
+++ opposite.js
@@ -0,0 +1,22 @@
+/*
+ * Author: Tyler Marenger
+ * Username: tjmareng 
+ * Profile: https://www.codewars.com/users/tjmareng
+ * Challenge: https://www.codewars.com/kata/opposite-number
+ */
+function opposite(number) {
+
+    // If the number is negative 
+    if (number < 0) {
+
+        // Return positive
+        return positiveNum = (number < 0) ? number * -1 : number;
+    }
+    // Otherwise
+    else {
+
+        // Return negative
+        return negativeNum = (number < 0) ? number : number * -1;
+    }
+
+}
