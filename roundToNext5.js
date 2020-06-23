c65b5a739e1cde6038c216fcedc6c7b524b40877
Tyler Marenger <tjmareng@mtu.edu>
2020-06-23 19:44:27 -0400

    Completed roundToNext5 Challenge

---
+++ roundToNext5.js
@@ -0,0 +1,9 @@
+/*
+ * Author: Tyler Marenger
+ * Username: tjmareng 
+ * Profile: https://www.codewars.com/users/tjmareng
+ * Challenge: https://www.codewars.com/kata/55d1d6d5955ec6365400006d
+ */
+function roundToNext5(n) {
+    return Math.ceil(n / 5) * 5;
+}
